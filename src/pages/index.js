586b62dc54340ca5b5666afbff2556249c26c97e
Heini Ahokas <ahokas.heini@gmail.com>
2019-03-17 13:44:09 +0100

    add park page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import "./index.css"
 
-export default () => <div>Heini's portfolio site</div>
+export default () => (
+    <div className="park-page-content-container">
+     <span className="park-page-content-image" role="img" aria-label="unicorn-emoji">🦄</span>
+     <span className="park-page-content-text">coming soon</span>
+    </div>
+)
